ab7c54881cf5ec8c781d8c528ab8494e4fd9a011
DESKTOP-LKD0OF1\user <twn005@gsd.net.tw>
2024-04-18 16:02:35 +0800

    feat: Navbar 數量呈現

--- component/navbarComponent.js
+++ component/navbarComponent.js
@@ -1,12 +1,18 @@
+const { mapState } = Pinia;
+import cartStore from "../store/cartStore.js"
+
 export default {
     template: `<nav class="navbar navbar-light bg-light">
     <div class="container-fluid">
         <span class="navbar-brand mb-0 h1">Navbar</span>
         <button type="button" class="btn">
             購物車
-            <span class="badge rounded-pill bg-danger">0</span>
+            <span class="badge rounded-pill bg-danger">{{ cart.length }}</span>
         </button>
     </div>
 </nav>
-    `
+    `,
+    computed: {
+        ...mapState(cartStore, ['cart'])
+    }
 }
